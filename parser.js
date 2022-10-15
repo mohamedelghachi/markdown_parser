69be5a3c9d31de49829d2c4f4433dfec4dab5452
fouad_el1 <felhamd@gmail.com>
2022-10-15 10:06:16 +0000

    @ : pour marquage du texte

--- parser.js
+++ parser.js
@@ -4,11 +4,12 @@ const markdownParser = (text) => {
         .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
         .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
         .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
-        .replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
+        .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+        .replace(/^@(.*$)/gim, '<mark>$1</mark>'); // marquage du texte
     return toHTML.trim(); // using trim method to remove whitespace
 }
 
-function converter(event){
+function converter(event) {
     event.preventDefault();
     var texterea_content = document.getElementById("originalText").value;
     var result = document.getElementById("div_result");
