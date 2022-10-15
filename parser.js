88a7956587af5889b9adc2d5a8cddf14b15ffbaf
salmaAgour <salmaag983@gmail.com>
2022-10-15 11:30:15 +0100

    Merge branch 'master' of https://github.com/mohamedelghachi/markdown_parser

--- parser.js
+++ parser.js
@@ -8,7 +8,9 @@ const markdownParser = (text) => {
         .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
         .replace(/([^\n]+)(\+)([^\n]+)/gim, "<ul><li>$3</li></ul>")// list
         .replace( /(")([^\n,])(")/gim,'<a style="background-color:grey;color:black;text-decoration: none;border-radius: 3px;padding:0 2px;">$2</a>')//highlights
-        .replace(/^@(.*$)/gim, '<mark>$1</mark>'); // marquage du texte
+        .replace(/^@(.*$)/gim, '<mark>$1</mark>') // marquage du texte
+        .replace(/\_(.*)\_/gim, '<u>$1</u>') // underlined text
+        .replace(/\-(.*)\-/gim, '<span style="text-decoration:line-through">$1</span>'); // line through text
 
     return toHTML.trim(); // using trim method to remove whitespace
 }
