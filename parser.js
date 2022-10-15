a67cd49c6299f082698d3fb2ed998842dd4890b9
kawtarsaiddoun <kawtarsaiddoun27@gmail.com>
2022-10-15 11:19:21 +0100

    add markdown

--- parser.js
+++ parser.js
@@ -4,7 +4,9 @@ const markdownParser = (text) => {
         .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
         .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
         .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
-        .replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
+        .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+        .replace(/([^\n]+)(\+)([^\n]+)/gim, "<ul><li>$3</li></ul>")//list
+        .replace( /(")([^\n,])(")/gim,'<a style="background-color:grey;color:black;text-decoration: none;border-radius: 3px;padding:0 2px;">$2</a>')//highlights
     return toHTML.trim(); // using trim method to remove whitespace
 }
 
