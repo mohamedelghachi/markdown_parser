f720c90406a1a6af9b8e3adfc0c584aa8ddf2cce
mohamed.elghachi <med.ghachi@gmail.com>
2022-10-15 10:05:13 +0100

    initialize project with 3 functionnalities

---
+++ parser.js
@@ -0,0 +1,18 @@
+const markdownParser = (text) => {
+    const toHTML = text
+        .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
+        .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
+        .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
+        .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+        .replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
+    return toHTML.trim(); // using trim method to remove whitespace
+}
+
+function converter(event){
+    event.preventDefault();
+    var texterea_content = document.getElementById("originalText").value;
+    var result = document.getElementById("div_result");
+    result.innerHTML = markdownParser(texterea_content);
+}
+
+//
